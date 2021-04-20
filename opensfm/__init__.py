95fa41e962c69da3be6e07cfdf33a7b856d7dee3
Piero Toffanin <pt@masseranolabs.com>
2021-04-20 18:08:50 -0400

    PoC OpenSfM working on Windows

---
+++ opensfm/__init__.py
@@ -0,0 +1,7 @@
+# import sys, os
+# if sys.platform == 'win32':
+#     modules_path = os.path.join(os.path.dirname(__file__), "Release")
+#     print(modules_path)
+#     sys.path.insert(0, modules_path)
+
+#     from Release import pyfeatures
